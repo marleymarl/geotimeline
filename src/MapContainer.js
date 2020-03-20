de3a9544052dd64320d9ea44404420a1d77e4335
Greg Marlin <greg@transformation.ai>
2020-03-20 15:34:23 -0400

    got InfoWindow to work on Marker Click

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -10,11 +10,18 @@ const map_style = {
 
 export class MapContainer extends Component {
 state = {
-footPrints: [{lat: 37.774929, lon: -122.419416}]
+footPrints: [{lat: 37.774929, lon: -122.419416}],
+activeMarker: {},
+showingInfoWindow: false,
+selectedPlace: {}
 }
 
-onMarkerClick = ({x, y, lat, lng, event}) => {
-console.log('Greg is awesome because: ' + x, y, lat, lng, event)
+onMarkerClick = (mapProps, map, clickEvent) => {
+console.log('Greg is awesome because: ' + JSON.stringify(clickEvent))
+}
+
+superMarkerClick = (markerProps, marker, clickEvent) => {
+	this.setState({selectedPlace: markerProps, activeMarker: marker, showingInfoWindow: true})
 }
 
 render() {
@@ -28,12 +35,34 @@ onClick={this.onMarkerClick}
 />)
 return(
 <Map google={this.props.google} style={map_style}
-intialCenter={{
+initialCenter={{
 lat: 43.6532, //change this to be set based on location input on form prior to map
-lng: 79.3832
+lng: -79.3832,
 }}
+onClick={this.onMarkerClick}
 >
-{currentMarkers}
+  <Marker
+    title={'The marker`s title will appear as a tooltip.'}
+    name={'Eaton Centre'}
+    position={{lat: 43.65509405622337, lng: -79.38795019216536}} 
+    onClick={this.superMarkerClick}/>
+    <Marker
+    title={'The marker`s title will appear as a tooltip.'}
+    name={'Fashion District'}
+    position={{lat: 43.64776590062451, lng: -79.41258359975814}} 
+    onClick={this.superMarkerClick}/>
+    <Marker
+    title={'The marker`s title will appear as a tooltip.'}
+    name={'Trinity Bellwoods'}
+    position={{lat: 43.64490891977491, lng: -79.40079684919975}} 
+    onClick={this.superMarkerClick}/>
+    <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
 </Map>
 );
 
