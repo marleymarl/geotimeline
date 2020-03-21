528ee78c92e1fb78affba15511abac8c4bf7f137
Greg Marlin <greg@transformation.ai>
2020-03-21 14:45:18 -0400

    last commit before colab

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,11 +1,19 @@
 import React, {Component } from 'react'; 
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+// import { Form, FormItem, Input, InputNumber, Checkbox, DatePicker, TimePicker } from "formik-antd";
+// import { Formik, ErrorMessage } from 'formik';
+import { DatePicker, TimePicker, Form, Button, Modal } from 'antd';
+import 'antd/dist/antd.css';
 
 var apiKey = 'AIzaSyA61clFhCrihwKKKsF8lz0SJ_jb32nhiXg'
 
 const map_style = {
   width: '90%',
   height: '100%',
+}
+
+function onChange(date, dateString) {
+	console.log(date, dateString)
 }
 
 export class MapContainer extends Component {
@@ -18,11 +26,19 @@ selectedPlace: {}
 
 onMarkerClick = (mapProps, map, clickEvent) => {
 console.log('Greg is awesome because: ' + JSON.stringify(clickEvent))
+let markerLatLng = clickEvent.latLng
 }
 
 superMarkerClick = (markerProps, marker, clickEvent) => {
 	this.setState({selectedPlace: markerProps, activeMarker: marker, showingInfoWindow: true})
+	//set state with activeMarker info, then use this.state.activeMarker.props to populate info for Modal
+	//change Modal visible state to true
+	//then take date and time data from the pickers, along with activeMarker props and push that as footprint into the footprint array in state
+	//on Component Save (needs to be parent component method) take footprint array and save it as a child prop of CaseTimeline Component
+	//CaseTimeline is parent of MapContainer which is parent of Map, Marker, Modal, Form, DatePicker, TimePicker
 }
+
+
 
 render() {
 const currentMarkers = this.state.footPrints.map((footprint) => <Marker
@@ -58,9 +74,15 @@ onClick={this.onMarkerClick}
     onClick={this.superMarkerClick}/>
     <InfoWindow
           marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
+          visible={this.state.showingInfoWindow}
+          maxWidth={800}>
             <div>
-              <h1>{this.state.selectedPlace.name}</h1>
+           <Form>
+              <DatePicker onChange={value => console.log(value)} />
+              <TimePicker />
+              <Button type="primary" onClick={() => { console.log('yo') }}>Save Footprint</Button>
+            </Form>
+            
             </div>
         </InfoWindow>
 </Map>
