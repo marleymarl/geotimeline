fa9229dc51211d3c9929dce3103490668fcea9e4
Dylan <dylan@dylandavenport.com>
2020-03-22 12:59:08 -0400

    Added form stlyes. May want to add text at the top with a description.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MapContainer from './MapContainer'
+// import MapContainer from './MapContainer'
+
+import UpFrontForm from './upFrontForm';
 
 function App() {
   return (
     <div className="App">
-      <MapContainer patientId={'1234567'}/>
+      <UpFrontForm patientId={'1234567'} />
     </div>
   );
 }
