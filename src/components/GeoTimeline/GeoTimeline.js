c9211658ec9c8acad6fc0ac9def04b63296487cb
Dylan <dylan@dylandavenport.com>
2020-03-28 13:58:29 -0400

    Formatted code in GeoTimeline component.

--- src/components/GeoTimeline/GeoTimeline.js
+++ src/components/GeoTimeline/GeoTimeline.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
-=======
-import React, {Component } from 'react'; 
->>>>>>> fd905233ec472d171e7a83e7c6f1f67aac5f2ae0
 
 import MapContainer from '../../MapContainer';
 import UpFrontForm from '../../upFrontForm';
@@ -10,7 +6,6 @@ import UpFrontForm from '../../upFrontForm';
 var apiKey = 'AIzaSyA61clFhCrihwKKKsF8lz0SJ_jb32nhiXg';
 
 export default class GeoTimeline extends Component {
-<<<<<<< HEAD
   state = {
     patientId: ''
   };
@@ -37,40 +32,3 @@ export default class GeoTimeline extends Component {
     return <div>{this.renderMapOrForm()}</div>;
   }
 }
-=======
-	state = {
-		patientId: ''
-	}
-
-	handlePatientIdSubmit = (patientId) => {
-		this.setState({patientId})
-	}
-
-	renderMapOrForm = () => {
-
-		if (this.state.patientId === '') {
-			return <UpFrontForm handlePatientIdSubmit={this.handlePatientIdSubmit} />
-		}
-		else {
-			return <MapContainer patientId={this.state.patientId} apiKey={apiKey} />
-		}
-
-	}
-
-
-
-	render() {
-
-		return (
-			<div>
-
-			{this.renderMapOrForm()}
-
-			</div>
-
-			)
-
-	}
-}
-
->>>>>>> fd905233ec472d171e7a83e7c6f1f67aac5f2ae0
