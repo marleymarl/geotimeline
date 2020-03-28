d5f72d34440f85e2444ca93296e60a5017ed47be
Dylan <dylan@dylandavenport.com>
2020-03-28 13:33:07 -0400

    Added props for form header and description as well as added background image.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MapContainer from './MapContainer'
+import MapContainer from './MapContainer';
+import GeoTime from './components/GeoTimeline/GeoTimeline';
 
 // import UpFrontForm from './upFrontForm';
 
 function App() {
   return (
     <div className="App">
-      <MapContainer patientId={'1234567'} />
+      <GeoTime />
     </div>
   );
 }
