cfed8dfa65b85614a5eb00964134c4c49d2f2bf0
Dylan <dylan@dylandavenport.com>
2020-04-04 23:55:42 -0400

    Uncommented a line I commented out for testing purposes.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-=======
-// import App from './App';
->>>>>>> Added props to upFrontButtonForm component for header and description. This will help explain to users what action needs to be taken on the page and what to expect when using this application.
 import MainRouter from './modules/MainRouter';
 import * as serviceWorker from './serviceWorker';
 
