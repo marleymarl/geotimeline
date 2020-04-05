1c09a6051a62749a6810e2240124ee61af3de8ec
Dylan <dylan@dylandavenport.com>
2020-04-04 23:45:01 -0400

    Added props to upFrontButtonForm component for header and description. This will help explain to users what action needs to be taken on the page and what to expect when using this application.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import MainRouter from './modules/MainRouter';
 import * as serviceWorker from './serviceWorker';
 
