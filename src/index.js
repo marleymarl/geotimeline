8c6682435819ec29ac86e7f12cee0daf4d4dee94
Helder Sepulveda <heldersepu@gmail.com>
2020-04-05 10:19:44 -0400

    Clean warning `'App' is defined but never used`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import MainRouter from './modules/MainRouter';
 import * as serviceWorker from './serviceWorker';
 
